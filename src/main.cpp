5028099c7f75251b8b6c11cde26332ac00cd7cb7
Francisco Gasca <francisco_gasca@hotmail.com>
2023-06-22 04:16:12 -0600

    Added code example for create a Window in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,33 @@
 #include <iostream>
 #include "SFML/Graphics.hpp"
 
+void draw(sf::RenderWindow& window)
+{
+    // Add drawing here
+}
+
 int main()
 {
-    std::cout << "Hola\n";
+    sf::RenderWindow window(sf::VideoMode(800, 600), "My Window");
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+            {
+                window.close();
+            }
+        }
+
+        window.clear(sf::Color::Black);
+
+        draw(window);
+
+        window.display();
+    }
+
+    return 0;
 }
